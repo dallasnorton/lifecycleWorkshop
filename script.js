ff3036a6850d200ff538d22a4e8cc7f486ac2fc6
Dallas Norton <dallas.norton@domo.com>
2015-06-25 16:48:11 -0600

    responsive centered svg

---
+++ script.js
@@ -0,0 +1,33 @@
+// var svg = d3.select('#responsiveContainer')
+//   .append('svg')
+//   .attr({
+//   	'width': 800,
+//   	'height': 800,
+//     'viewBox': '0 0 800 800'
+//   });
+
+// svg.append('rect')
+//   .attr({
+//     'width': '100',
+//     'height': '200'
+//   });
+
+
+var svg = d3.select('#responsiveContainer')
+  .append('svg')
+  .attr({
+    'viewBox': "0 0 800 800",
+    'preserveAspectRatio': "xMinYMin meet"
+  });
+
+svg.append('rect')
+  .attr({
+    'width': '100',
+    'height': '200'
+  });
+
+svg.append('text')
+  .attr({
+    'transform': 'translate(300,300)',
+  })
+  .text('TEST');
